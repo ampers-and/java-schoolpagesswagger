a50e468aa186f85f868c2372b8e6b84677b86ca2
ampersand <hossainazifa@gmail.com>
2019-10-16 10:41:57 -0700

    Stretch

--- school/src/main/java/com/lambdaschool/school/service/InstructorServiceImpl.java
+++ school/src/main/java/com/lambdaschool/school/service/InstructorServiceImpl.java
@@ -1,12 +1,53 @@
 package com.lambdaschool.school.service;
 
+import com.lambdaschool.school.model.Instructor;
 import com.lambdaschool.school.repository.InstructorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service(value = "instructorService")
 public class InstructorServiceImpl implements InstructorService
 {
     @Autowired
     private InstructorRepository instructrepos;
+
+    @Override
+    public void delete(long id) throws EntityNotFoundException
+    {
+        if (instructrepos.findById(id).isPresent())
+        {
+            instructrepos.deleteById(id);
+        } else
+        {
+            throw new EntityNotFoundException(Long.toString(id));
+        }
+    }
+
+    @Transactional
+    @Override
+    public Instructor save(Instructor instructor)
+    {
+        Instructor newInstructor = new Instructor();
+
+        newInstructor.setInstructname(instructor.getInstructname());
+
+        return instructrepos.save(newInstructor);
+    }
+
+    @Override
+    public Instructor update(Instructor instructor, long id)
+    {
+        Instructor currentInstructor = instructrepos.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Long.toString(id)));
+
+        if (instructor.getInstructname() != null)
+        {
+            currentInstructor.setInstructname(instructor.getInstructname());
+        }
+
+        return instructrepos.save(currentInstructor);
+    }
 }
